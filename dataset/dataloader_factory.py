5203ec08e0df9449d020938662b767ebaa1edc08
Haruto Ishikawa <h.ishikawa.797@nitech.jp>
2026-05-07 16:11:26 +0900

    AVADetectionDatasetのサポートを追加し，データローダーの設定を更新

--- dataset/dataloader_factory.py
+++ dataset/dataloader_factory.py
@@ -12,12 +12,16 @@ from dataset import (
     sequential_video_folder,
     epic_kitchens_sequential_data_folder,
     EpicKitchensSequentialDataFolderInfo,
+    ava_sequential_data_folder,
+    AvaSequentialDataFolderInfo,
     transform_image,
     TransformImageInfo,
     transform_video,
     TransformVideoInfo,
     build_epic_kitchens_sequential_transform,
+    build_ava_sequential_transform,
 )
+from model.memvit.config.defaults import get_cfg
 
 
 @dataclass
@@ -34,19 +38,21 @@ class DataloadersInfo:
 
 
 SupportedDatasets = Literal["ImageFolder", "VideoFolder",
-                            "SequentialVideoFolder", "EpicKitchenSequentialDataset"]
+                            "SequentialVideoFolder", "EpicKitchenSequentialDataset",
+                            "AVADetectionDataset"]
 
 
 def configure_dataloader(
     command_line_args: argparse.Namespace,
     dataset_name: SupportedDatasets,
+    cfg=None,
 ):
     """dataloader factory
 
     Args:
         command_line_args (argparse.Namespace): command line args
         dataset_name (SupportedDatasets): dataset name (str).
-            ["ImageFolder", "VideoFolder", "SequentialVideoFolder", "EpicKitchenSequentialDataset"]
+            ["ImageFolder", "VideoFolder", "SequentialVideoFolder", "EpicKitchenSequentialDataset", "AVADetectionDataset"]
 
     Raises:
         ValueError: invalid dataset_name is given
@@ -131,6 +137,30 @@ def configure_dataloader(
                     label_type=args.epic_label_type,
                     background_label=args.background_label,
                     anticipation_time=args.epic_anticipation_time,
+                ))
+
+    elif dataset_name == "AVADetectionDataset":
+        if cfg is None:
+            cfg = get_cfg()
+            if args.cfg_file is not None:
+                cfg.merge_from_file(args.cfg_file)
+            if args.opts is not None:
+                cfg.merge_from_list(args.opts)
+
+        train_transform, val_transform = \
+            build_ava_sequential_transform(TransformVideoInfo(
+                frames_per_clip=cfg.DATA.NUM_FRAMES,
+                val_shorter_side_size=cfg.DATA.TEST_CROP_SIZE,
+                crop_size=cfg.DATA.TRAIN_CROP_SIZE,
+            ))
+        train_loader, val_loader, n_classes = \
+            ava_sequential_data_folder(
+                AvaSequentialDataFolderInfo(
+                    cfg=cfg,
+                    batch_size=args.batch_size,
+                    num_workers=args.num_workers,
+                    train_transform=train_transform,
+                    val_transform=val_transform,
                 ))
 
     else:
