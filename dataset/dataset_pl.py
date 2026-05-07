d15b0c50b1e3f480a0951277e1833e6126d4b4f6
Haruto Ishikawa <h.ishikawa.797@nitech.jp>
2026-05-07 16:12:14 +0900

    cfg引数をTrainValDataModuleのコンストラクタに追加し，configure_dataloaderの呼び出しを更新

--- dataset/dataset_pl.py
+++ dataset/dataset_pl.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import Any
 
 import lightning.pytorch as pl
 
@@ -10,6 +11,7 @@ class TrainValDataModule(pl.LightningDataModule):
         self,
         command_line_args: argparse.Namespace,
         dataset_name: str,
+        cfg: Any = None,
     ):
         super().__init__()
         self.args = command_line_args
@@ -17,7 +19,8 @@ class TrainValDataModule(pl.LightningDataModule):
         self.dataloaders_info = \
             configure_dataloader(
                 command_line_args=command_line_args,
-                dataset_name=dataset_name  # type: ignore[arg-type]
+                dataset_name=dataset_name,  # type: ignore[arg-type]
+                cfg=cfg,
             )
 
     def train_dataloader(self):
